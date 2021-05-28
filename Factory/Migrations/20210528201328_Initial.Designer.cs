57caf55b56717eb8183489c78964792674f3a498
Patrick Lee <patricklee1138@live.com>
2021-05-28 13:16:12 -0700

    redo Initial migration; update Views

--- Factory/Migrations/20210528201328_Initial.Designer.cs
+++ Factory/Migrations/20210528201328_Initial.Designer.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Factory.Migrations
 {
     [DbContext(typeof(FactoryContext))]
-    [Migration("20210528184312_Initial")]
+    [Migration("20210528201328_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
