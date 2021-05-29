c2315b8fdde65699b83fa4bc2c66e4fd6da46d27
Patrick Lee <patricklee1138@live.com>
2021-05-29 10:38:39 -0700

    add jQuery dependency; adjust UI/UIX

---
+++ Factory/wwwroot/js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+  $(".engineer-name").val("Bender Bending Rodriguez");
+  $(".machine-name").val("Fing-Longer");// https://futurama.fandom.com/wiki/Category:Inventions
+});
